7605c00b4272fab4231a778a1066e37566e2542a
Pratik <pratikchandlekar280@gmail.com>
2022-04-20 00:42:53 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  console.log(__dirname);
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+const uri = process.env.ATLAS_URI;
+
+mongoose
+  .connect(uri, {
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(console.error);
+
+// Models
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+
+  res.json({ result });
+});
+
+app.get("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.complete = !todo.complete;
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.put("/todo/update/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.text = req.body.text;
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
