a3f0dcfce40b2f30671e2a06e3633b6b4c436c25
Pratik <pratikchandlekar280@gmail.com>
2022-04-20 01:21:22 +0530

    folder structure changed

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,12 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
   console.log(__dirname);
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "client", "build", "index.html")
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
     )
   );
 } else {
